764f83826b36a98d8cf1793c03910e03f990b4bd
Chima Chibueze <39227698+ChimaTech@users.noreply.github.com>
2022-08-19 16:50:49 +0100

    Add initial files via upload
    Add the main shell script "amend-DuplicateFilename(-fileExtn-testName)(ver.01).sh", Read-Me file and sample mp3 files.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# amend-a-DuplicateFilename
-The shell script `amend-DuplicateFilename(-fileExtn-testName)(ver.01).sh` recursively checks if there is a duplication of filename and prints an appropriate filename.
+# Amend a Duplicate Filename.
+
+### Inputs : 
+- fileExtension *(string, 1st argument)*
+- initial_testName *(string, 2nd argument)*
+
+### Output: 
+- filename *(string)*
+
+### Output Format: 
+- string
+
+### Description. 
+The shell script `amend-DuplicateFilename(-fileExtn-testName)(ver.01).sh` recursively checks if there is a duplication of filename and prints an appropriate filename. 
+
+It compares the **initial_testName** with existing filenames for each file with the file extension **fileExtension**. 
+
+ It then prints:
+ - `$initial_testName-$appendixString`, if **initial_testName** already exist, or
+ - `$initial_testName`, if **initial_testName** doesn't already exist. 
+
+### HOW TO RUN. 
+Example: 
+```sh
+bash "amend-DuplicateFilename(-fileExtn-testName)(ver.01).sh" "mp3" "Obi"
+```
---
+++ amend-DuplicateFilename(-fileExtn-testName)(ver.01).sh
@@ -0,0 +1,92 @@
+#! /bin/bash
+
+# Description:
+# INPUTS: (x) fileExtension
+#+        (x) initial_testName
+
+#OUTPUT: filename
+
+#OUTPUT FORMAT: string
+
+# HOW TO RUN example: 
+#+ $ bash "thisScript.sh" "fileExtension" "initial_testName"
+
+# Function:: to recursively check if there is a duplication of filename.
+#+ compares initial_testName with existing filenames in *.fileExtension
+#+ prints $initial_testName-$appendixString, if initial_testName already exist.
+#+ prints $initial_testName, if initial_testName doesn't already exist
+# Declaring the main function
+Duplication_Amendment() {
+    
+    fileExtension="$1"
+    
+    initial_testName="$2"
+    newTestName=""
+    
+    # Delete previous functionReport
+    functionReport="functionReport.txt"
+    rm -rf "${functionReport}"
+    
+    # Declaring the nested function
+    Duplication_Amendment_subFunc() {
+        
+        fileExtension="$1"
+        
+        testName="$2" # testName will initially be == $initial_testName
+                      # testName will change, if testName == $itemName_inFocus
+                      # ...d/4...will be appended with "-copy" 
+                      # to be assigned to a newTestName
+        
+        for y in *."${fileExtension}"
+            do
+            
+                itemFullName_inFocus="${y##*/}"
+                itemName_inFocus="${itemFullName_inFocus%.*}"
+                
+                if [[ "${2,,}" == "${itemName_inFocus,,}" ]]; then
+                    #IF: $2 == $itemName_inFocus, regardless of the case. 
+                    # NB: ${var,,} == all-lower-case version of $var
+                    # NB: ${var^^} == all-upper-case version of $var 
+                    
+                    appendixString="(1)" # "(1)" | "copy"
+                    
+                    newTestName="${2}-${appendixString}"
+                    
+                    # Make this nested function call itself recursively
+                    #+ if $2 == $itemName_inFocus, 
+                    #+ until the $2 != $itemName_inFocus
+                    Duplication_Amendment_subFunc "${1}" "${newTestName}"
+                    
+                fi
+            
+            # functionReport to Report the behaviour of the function
+            #echo "newName_stage2: ${initial_testName} 
+            #     | < testName @subFunc = ${testName} >
+            #        itemFullName_inFocus: ${itemFullName_inFocus} | itemName_inFocus: ${itemName_inFocus}
+            #        [ newName_stage3 == ${newTestName} ]
+            #        " >> "${functionReport}"
+                    
+            done
+            
+        }
+    
+    # Call the Duplication_Amendment_subFunc
+    #+ since calling the main Duplication_Amendment
+    #+ doesn't automatically call the nested Duplication_Amendment_subFunc
+    Duplication_Amendment_subFunc "$1" "$2"
+    
+    if [[ "${newTestName}" == "" ]]; then
+        # IF: newTestName is still an empty string
+        echo "${initial_testName}"
+        
+    else
+        echo "${newTestName}"
+    
+    fi
+    
+} 
+
+# Call the Duplication_Amendment function, now. 
+Duplication_Amendment "$1" "$2"
+
+# test
